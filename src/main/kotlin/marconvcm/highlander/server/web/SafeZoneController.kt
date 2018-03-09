cd03599598d97227a8ee236683d65915bac31a3c
Marcos Vinicius Marcon <marconvcm@gmail.com>
2018-03-09 10:03:06 -0300

    Change pathVariable pattern to match SpringBoot defaults

--- src/main/kotlin/marconvcm/highlander/server/web/SafeZoneController.kt
+++ src/main/kotlin/marconvcm/highlander/server/web/SafeZoneController.kt
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.*
 class SafeZoneController(
     @Autowired val safeZoneService: SafeZoneService) {
 
-    @PutMapping("/open/:personId")
+    @PutMapping("/open/{personId}")
     fun open(@PathVariable("personId") personId: String): SafeZone = safeZoneService.open(personId)
 
-    @DeleteMapping("/close/:token")
+    @DeleteMapping("/close/{token}")
     fun close(@PathVariable("token") token: String): String = safeZoneService.close(token)
 
-    @GetMapping("/keepalive/:token")
+    @GetMapping("/keepalive/{token}")
     fun keepAlive(@PathVariable("token") token: String): String = safeZoneService.keepAlive(token)
 }
